9c559fa00376da63e994df607645458f71966bd5
Bayez77 <mrbayez88@gmail.com>
2025-02-13 19:51:56 -0600

    Tags Ticket Completed

---
+++ Models/Tags.cs
@@ -0,0 +1,33 @@
+namespace Rare;
+
+public class Tags
+{
+    public int Id {get; set;}
+
+    public string Label {get; set;}
+
+}
+
+    public class TagsObj
+    {
+        public static Tags tags1 = new Tags
+        {
+            Id = 1000001,
+            Label = "Read",
+        };
+
+         public static Tags tags2 = new Tags
+        {
+            Id = 1000002,
+            Label = "Responded",
+        };
+
+         public static Tags tags3 = new Tags
+        {
+            Id = 1000003,
+            Label = "Unread",
+        };
+
+        public static List<Tags> tags = new List<Tags> 
+        {tags1, tags2, tags3};
+    }
