ab07666c3da9b438484d21121961ccca9d1faa5b
Christina Vieau <cvieau@live.com>
2025-02-15 11:51:31 -0500

    deleted weather info

--- Program.cs
+++ Program.cs
@@ -18,32 +18,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
 
 
 //users Endpoints
@@ -111,3 +87,5 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 
 
 //Subscriptions Endpoints
+
+app.Run();
